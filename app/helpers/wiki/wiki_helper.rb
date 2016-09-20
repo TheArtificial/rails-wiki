89011329361566976df819c19bea67dbb8296436
Hans Gerwitz <robots@theartificial.nl>
2016-09-20 09:15:25 +0200

    wiki_link helper

--- app/helpers/wiki/wiki_helper.rb
+++ app/helpers/wiki/wiki_helper.rb
@@ -5,5 +5,14 @@ module Wiki
       # limit: max updates
       render partial: 'wiki/updates', locals: {updates: Rails.configuration.wiki.recent_updates(options), options: options}
     end
+
+    def wiki_link(path)
+      page = Rails.configuration.wiki.find_page(path)
+      if page.new_page?
+        return path
+      else
+        return link_to page.name, wiki.page_path(page)
+      end
+    end
   end
 end
