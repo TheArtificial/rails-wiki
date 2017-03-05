7d937c56075d930231a11b071d44f30b4c38bc9f
Hans Gerwitz <hans@gerwitz.com>
2017-03-05 19:28:11 +0100

    Better path handling

--- lib/wiki_links.rb
+++ lib/wiki_links.rb
@@ -6,23 +6,20 @@ class Gollum::Filter::WikiLinks < Gollum::Filter
   def extract(data)
     wiki = Rails.configuration.wiki
     this_page = Rails.configuration.wiki.find_page(@markup.page.url_path)
-    context_path = this_page.parent_path
-    Rails.logger.debug ("context_path: #{context_path}")
+    context_path = this_page.path
 
     data.gsub(/\[(.+?)\][^\(]/) do
       path = $1
-      Rails.logger.debug ("path: #{path}")
       if path.starts_with?('/')
         resolved_path = path[1..-1]
       else
         resolved_path = context_path + '/' + path
       end
-      Rails.logger.debug ("resolved_path: #{resolved_path}")
       page = wiki.find_page(resolved_path)
       if page.nil? || page.new_page?
-        "<a href=\"#{resolved_path}\">[#{resolved_path}]</a>"
+        "[<a href=\"#{Wiki::Engine.routes.url_helpers.new_page_path(resolved_path)}\">create #{path}</a>]"
       else
-        "<a href=\"#{Wiki::Engine.routes.url_helpers.page_path(page)}\">#{page.name}</a>"
+        "<a href=\"#{Wiki::Engine.routes.url_helpers.page_path(page)}\">#{page.title}</a>"
       end
     end
   end
