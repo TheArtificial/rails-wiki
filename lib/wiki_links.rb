79ab5956999b04c260a81aef7072dc6af382fb54
Hans Gerwitz <hans@gerwitz.com>
2017-03-06 22:04:57 +0100

    new internal link style

--- lib/wiki_links.rb
+++ lib/wiki_links.rb
@@ -2,18 +2,18 @@ class Gollum::Filter::WikiLinks < Gollum::Filter
   # include RailsWiki::Engine.routes.url_helpers
   # include ActionView::Helpers::UrlHelper
 
-  # replace [/path] with [/prefix/path]
+  # process [[page/path]] into proper links
   def extract(data)
     this_page = Rails.configuration.wiki.find_page(@markup.page.url_path)
 
-    data.gsub(/\[(.+?)\][^\(]/) do
+    data.gsub(/\[\[(.+?)\]\]/) do
       path = $1.gsub(/\s/, '-')
       page = this_page.relative_page(path)
 
       if page.nil? || page.new_page?
-        "[<a href=\"#{Wiki::Engine.routes.url_helpers.new_page_path(page)}\">create #{path}</a>]"
+        "[<a href=\"#{Wiki::Engine.routes.url_helpers.new_page_path(page)}\" class=\"wikilink new\">create #{path}</a>]"
       else
-        "<a href=\"#{Wiki::Engine.routes.url_helpers.page_path(page)}\">#{page.title}</a>"
+        "<a href=\"#{Wiki::Engine.routes.url_helpers.page_path(page)}\" class=\"wikilink new\">#{page.title}</a>"
       end
     end
   end
